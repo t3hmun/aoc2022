8dc18d2a1d465df2fbe21a4d30a86d91d2f8e47d
t3hmun <git@t3hmun.com>
2022-12-01 18:47:50 +0000

    I do not know rust. The features nice.

--- rust/aoc01/src/main.rs
+++ rust/aoc01/src/main.rs
@@ -1,3 +1,10 @@
 fn main() {
-    println!("Hello, world!");
+    let data = include_str!("./data.txt")
+        .replace("\r", "")
+        .split("\n\n")
+        .map(|g| g.split("\n").map(|e| e.parse::<i64>().expect("parse fail")));
+
+    for x in data {
+        println!("{x}");
+    }
 }
