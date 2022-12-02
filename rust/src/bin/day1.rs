b46edaa56cc44a2e5ed75db8d8e4c8d6dbae3472
t3hmun <git@t3hmun.com>
2022-12-02 16:31:25 +0000

    Re-wrote using simple loop.

--- rust/src/bin/day1.rs
+++ rust/src/bin/day1.rs
@@ -1,17 +1,50 @@
+/* This is the first Rust I've ever written so maximum qualification and annotations.
+* Random notes:
+* - Reading the first 5 chapters of the Rust Book was essential
+* - The types for strings
+    - Not magical like they are in C# and JS.
+    - Not ASCII char array like in C
+    - UTF-8 byte array, len might be different to number of letters
+    - Different types for compiled literals (str) and runtime growable strings (Strings)
+*/
+
 fn main() {
     let data: String = std::fs::read_to_string("../day-01-data.txt").expect("Failed to read file.");
+    for_loops(&data);
+    map_reduce(&data);
+}
 
-    // Alt approach, Rust lets you compile in strings:
-    //let data: &str = include_str!("./day1-data.txt");
-    let elves = data.split("\n\n");
+/// This is my instinctive way to do it but understanding types returned by each function isn't obvious.
+fn map_reduce(data: &String) {
+    let ans_a = "todo";
+    let ans_b = "todo";
 
-    let mut parsed_elves: Vec<u32> = elves
-        .map(|line| line.split("\n").flat_map(|s| s.parse::<u32>()).sum())
-        .collect();
-    parsed_elves.sort_by(|a, b| b.cmp(a));
-    let ans_one = parsed_elves.iter().max().expect("no data?");
-    //let ans_two = parsed_elves.iter().take(3).sum();
+    println!("Answer A: {ans_a}, Answer B: {ans_b} (using the functional approach)");
+}
 
-    println!("One: {ans_one}");
-    //println!("Two: {ans_two}");
+/// The simple for loop approach, the types a simpler here. I don't like this though.
+fn for_loops(data: &String) {
+    let lines: std::str::Lines = data.lines();
+
+    let mut elf_total = 0;
+    let mut totals: Vec<u32> = Vec::new();
+
+    for line in lines {
+        if line.trim().len() == 0 {
+            totals.push(elf_total);
+            elf_total = 0;
+        } else {
+            let num: u32 = line.parse().expect("Failed to parse a number?");
+            elf_total += num;
+        }
+    }
+
+    totals.push(elf_total);
+
+    totals.sort_by(|a, b| b.cmp(a));
+
+    let ans_a: u32 = *(totals.first().expect("no data?"));
+    let ans_b: u32 = totals.iter().take(3).sum::<u32>();
+
+    println!("Answer A: {ans_a}, Answer B: {ans_b} (using the for-loop approach)");
 }
