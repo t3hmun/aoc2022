4bbd70dcf50e9bd416d8bb591cc72695d1d85edf
t3hmun <git@t3hmun.com>
2022-12-03 12:05:26 +0000

    day 2 part 2 rust.

--- rust/src/bin/day2.rs
+++ rust/src/bin/day2.rs
@@ -21,5 +21,27 @@ fn main() {
             score + points
         })
         .sum();
-    println!("Part One: {part_one}");
+
+    let part_two: i32 = data
+        .lines()
+        .map(|line| {
+            let score = match line {
+                "A Y" | "B X" | "C Z" => 1,
+                "A Z" | "B Y" | "C X" => 2,
+                "A X" | "B Z" | "C Y" => 3,
+                _ => panic!("Invalid input"),
+            };
+            // Rust won't index a UTF-8 string unless you as_bytes it.
+            // Reading UTF-8 code points is an forwards iterative process, hence nth.
+            let you = line.chars().nth(2).unwrap();
+            let points = match you {
+                'X' => 0,
+                'Y' => 3,
+                'Z' => 6,
+                _ => panic!("Invalid input"),
+            };
+            score + points
+        })
+        .sum();
+    println!("Part One: {part_one}, Part Two: {part_two}");
 }
