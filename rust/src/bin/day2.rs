f22540f096c297d472e9ee39ee99b8aed6f2c092
t3hmun <git@t3hmun.com>
2022-12-03 12:02:26 +0000

    Day 2 part one in Rust is simple.

---
+++ rust/src/bin/day2.rs
@@ -0,0 +1,25 @@
+fn main() {
+    let data = std::fs::read_to_string("../day-02-data.txt").unwrap();
+    let part_one: i32 = data
+        .lines()
+        .map(|line| {
+            let score = match line {
+                "A Y" | "B Z" | "C X" => 6,
+                "A Z" | "B X" | "C Y" => 0,
+                "A X" | "B Y" | "C Z" => 3,
+                _ => panic!("Invalid input"),
+            };
+            // Rust won't index a UTF-8 string unless you as_bytes it.
+            // Reading UTF-8 code points is an forwards iterative process, hence nth.
+            let you = line.chars().nth(2).unwrap();
+            let points = match you {
+                'X' => 1,
+                'Y' => 2,
+                'Z' => 3,
+                _ => panic!("Invalid input"),
+            };
+            score + points
+        })
+        .sum();
+    println!("Part One: {part_one}");
+}
