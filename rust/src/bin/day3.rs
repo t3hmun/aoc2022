6c95a6b870fcf58013d7d5a61980376e72d5311c
t3hmun <git@t3hmun.com>
2022-12-04 15:31:23 +0000

    Day 3 part 1 rust.

--- rust/src/bin/day3.rs
+++ rust/src/bin/day3.rs
@@ -1,13 +1,42 @@
 fn main() {
-    let data = std::fs::read_to_string("../day-03-test.txt").unwrap();
-    let part_one = data
+    let data = std::fs::read_to_string("../day-03-data.txt").unwrap();
+    let part_one: u32 = data
         .lines()
         .map(|line| {
             let bytes = line.as_bytes();
+            let side_len = bytes.len() / 2;
+            let left = &bytes[0..side_len];
+            let right = &bytes[side_len..];
+            let mut both = Vec::<u8>::new();
 
-            println!("{:?}", bytes);
+            for bl in left {
+                for br in right {
+                    if bl == br {
+                        both.push(*bl);
+                    }
+                }
+            }
+            both.sort();
+            both.dedup();
+            // A=65 Z=90 a=97 z=122
+            let priorities = both
+                .iter()
+                .map(|b| match b {
+                    65..=90 => *b as u32 - 38,
+                    97..=122 => *b as u32 - 96,
+                    _ => panic!("Unexpected byte val"),
+                })
+                .collect::<Vec<u32>>();
 
-            bytes
+            let total_pri: u32 = priorities.iter().sum();
+
+            //println!("\nBoth: {:?}", both);
+            //println!("PRi: {:?}", priorities);
+            //println!("total: {:?}", total_pri);
+
+            total_pri
         })
-        .collect::<Vec<&[u8]>>();
+        .sum();
+
+    println!("part one: {part_one}");
 }
