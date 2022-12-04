51d1382c916a692f9b8578fbed526bb814ee44de
t3hmun <git@t3hmun.com>
2022-12-04 18:04:36 +0000

    day 3 part 2 rust.

--- rust/src/bin/day3.rs
+++ rust/src/bin/day3.rs
@@ -1,7 +1,55 @@
 fn main() {
     let data = std::fs::read_to_string("../day-03-data.txt").unwrap();
-    let part_one: u32 = data
-        .lines()
+
+    let part_one = answer_part_one(&data);
+    let part_two = answer_part_two(&data);
+    println!("part one: {part_one}, part two: {part_two}");
+}
+
+fn answer_part_two(data: &String) -> u32 {
+    let lines = data.lines().collect::<Vec<&str>>(); // .array_chunks(); chunks is experimental :(
+
+    let num_of_lines = lines.len();
+
+    println!("num {:?}", num_of_lines);
+    let mut total: u32 = 0;
+
+    // array_chunks would have been nicer but that's experimental.
+    for i in (0..num_of_lines).step_by(3) {
+        let group = &lines[i..i + 3];
+        let first = group[0];
+
+        // My first Option.
+        let mut in_all = None;
+        let chars = first.chars();
+        for char in chars {
+            let is_in_all = group
+                .iter()
+                .skip(1)
+                .map(|line| line.chars().any(|c| c == char))
+                .all(|b| b == true);
+
+            if is_in_all == true {
+                in_all = Some(char);
+                break;
+            }
+        }
+
+        let b = in_all.unwrap() as u8;
+        let pri = match b {
+            65..=90 => b as u32 - 38,
+            97..=122 => b as u32 - 96,
+            _ => panic!("Unexpected byte val"),
+        };
+
+        total += pri;
+    }
+
+    total
+}
+
+fn answer_part_one(data: &String) -> u32 {
+    data.lines()
         .map(|line| {
             let bytes = line.as_bytes();
             let side_len = bytes.len() / 2;
@@ -36,7 +84,5 @@ fn main() {
 
             total_pri
         })
-        .sum();
-
-    println!("part one: {part_one}");
+        .sum()
 }
